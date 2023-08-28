c1521134342df1dadd22a060b42b74921cfa78d5
KimHyeonBong <vongright@gmail.com>
2023-08-28 16:25:26 +0900

    first commit.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function Food({name, picture, rating}) {
   return (
   <div>
     <h3>I love {name}</h3>
-    <h2>{rating} / 5.0</h2>
+    <h2>born in {born}</h2>
     <img src={picture} alt={name} width="300px"/>
   </div>
   );
@@ -13,24 +13,24 @@ function Food({name, picture, rating}) {
 
 const foodILike = [
   {id:1,
-    name : ' Kimch',
-   image : 'https://img.choroc.com/newshop/goods/024444/024444_1.jpg',
-  rating : 4.5
+    name : 'Pharrell Williams',
+   image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Pharrell_Williams_at_The_Lion_King_European_Premiere_2019.png/440px-Pharrell_Williams_at_The_Lion_King_European_Premiere_2019.png',
+    born : 'April 5, 1973 (age 50) Virginia Beach, Virginia, U.S.'
   },
   {id:2,
-    name : 'yuk-hoi',
-   image : 'https://i.namu.wiki/i/yUng9QLAEuvdSFliWKSnXGpNV_XzcAQG_LpkZMvCQ5tWB4rvyKAODwn8QQ_oStMABpQJv4B7uz_dULefC9E7OhVRXrfr-FPYHD07acR4r2_RTrg7M2-3vuBXPNzrCS5UBh6G4YSBGq5HOZx9JOzJxg.webp',
-  rating : 3.6
+    name : 'A$ap Rocky',
+   image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/South_by_Southwest_2019_4_-_46628270064_%28cropped%29.jpg/440px-South_by_Southwest_2019_4_-_46628270064_%28cropped%29.jpg',
+    born : 'October 3, 1988 (age 34) New York City, U.S.'
   },
   {id:3,
-    name : '',
-   image : 'https://w7.pngwing.com/pngs/412/821/png-transparent-apple-logo-apple-logo-company-heart-logo.png',
-  rating : 3.0
+    name : 'Tyler, the Creator',
+   image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Tyler_the_Creator_%2852163761341%29_%28cropped%29.jpg/440px-Tyler_the_Creator_%2852163761341%29_%28cropped%29.jpg',
+    born : 'March 6, 1991 (age 32) Hawthorne, California, U.S.'
   },
   {id:4,
-    name : 'jjigae',
-   image : 'https://i.ytimg.com/vi/PH_-nGRatgo/maxresdefault.jpg',
-  rating : 4.8
+    name : 'Lucky Daye',
+   image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Lucky-Daye_Ap-Images-604x800.jpg/440px-Lucky-Daye_Ap-Images-604x800.jpg',
+    born : 'September 25, 1985 (age 37) New Orleans, Louisiana, U.S.'
   }
 ];
 
@@ -44,8 +44,8 @@ function App() {
   return (
     <div>
       
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} rating={dish.rating} />)}
-      {foodILike.map(dish => <renderFood />)}
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} born={dish.born} />)}
+      
 
     </div>
   );
